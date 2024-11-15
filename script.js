dea55571b9e828b578f3fed85fd7ed886527269e
Mehedi-m <djm4223@gmail.com>
2024-11-16 00:15:03 +0600

    Pet care section added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function showPage(showId, hideId) {
     document.getElementById(showId).classList.remove('hidden');
     document.getElementById(showId).classList.add('block');
-    document.getElementById(hideId).classList.remove('block');
-    document.getElementById(hideId).classList.add('hidden');
+
+    hideId.forEach(hideId => {
+      document.getElementById(hideId).classList.add('hidden');
+      document.getElementById(hideId).classList.remove('block');
+    });
   }
